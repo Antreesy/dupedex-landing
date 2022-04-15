e124a79bb9564f904d8f89082afe60f3a15525be
Taras Shevchenko <shev-taras@ya.ru>
2022-04-16 01:09:06 +0400

    error: error 401

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
-const myPhone = document.getElementById('phone');
-const button = document.getElementById('button');
-const rex = /^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/;
-
-
-button.addEventListener('click', (event)=>{
-    event.preventDefault();
-    validPhone();
+document.querySelector('.button').addEventListener('click', (e)=>{
+    e.preventDefault();
+    const url = 'https://rest.clicksend.com/v3/sms/send'
+    const userName = 'raycai@hotmail.co.nz'
+    const apiKey = '9D54BDB7-A845-6768-CE45-24D20616F883'
+    const phone = document.querySelector('.phone').value
+    console.log(document.querySelector('.phone').value)
+    const options = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==",
+            "USERNAME": "raycai@hotmail.co.nz",
+            "API_KEY": "9D54BDB7-A845-6768-CE45-24D20616F883"
+        },
+        body: JSON.stringify({
+            phone,userName,apiKey
+            })
+    }
+    fetch(url, options)
+        .then(response => response.json())
+        .then((result) => {
+            return console.log(result)})
 })
 
-const validPhone = () => {
-    const valid = rex.test(myPhone.value);
-    console.log(valid)
-    valid ? output = '' : output = 'Номер введен неправильно';
-    document.getElementById('message').innerHTML = output;
-}
+
+
+
+// const rex = /^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/;
+//
+// const validPhone = () => {
+//     const valid = rex.test(myPhone.value);
+//     console.log(valid)
+//     valid ? output = '' : output = 'Номер введен неправильно';
+//     document.getElementById('message').innerHTML = output;
+// }
 
